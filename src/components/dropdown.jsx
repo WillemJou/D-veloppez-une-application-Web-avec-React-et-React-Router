7ee624ca1e9c0249a662f2c12726d5a927f24749
Willem Jouret <willem.jouret44@gmail.com>
2022-11-23 17:11:16 +0100

    Created dropdown component, add to page "A Propos"

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,28 @@
+import "../styles/dropdown.css"
+import { useState } from "react"
+import chevron from "../assets/chevron.svg"
+
+function DropdownMenu({ title, content }) {
+  const [open, setOpen] = useState(false)
+  const [rotateChevron, setRotateChevron] = useState(false)
+  const rotate = rotateChevron ? "rotate(180deg)" : "rotate(0)"
+  const handleOpen = () => {
+    return [setRotateChevron(!rotateChevron), setOpen(!open)]
+  }
+  return (
+    <div className="main-container">
+      <button className="banner" onClick={() => handleOpen()}>
+        {title}
+        <img
+          className="chevron"
+          style={{ transform: rotate, transition: "0.5s" }}
+          src={chevron}
+          alt="chevron"
+        />
+      </button>
+      {open ? <div className="main-content">{content}</div> : null}
+    </div>
+  )
+}
+
+export default DropdownMenu
--- src/containers/pages/APropos.jsx
+++ src/containers/pages/APropos.jsx
@@ -1,8 +1,59 @@
 import Header from '../layout/header';
 import imgapropos from '../../assets/imgapropos.png'
+import DropdownMenu from '../../components/dropdown'
 import Footer from '../layout/footer'
 import '../../styles/accueil.css'
 import '../../styles/index.css'
+
+function Fiabilité() {
+  return (
+    <div className="main-container">
+      <DropdownMenu
+        title="Fiabilité"
+        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+      />
+    </div>
+  )
+}
+
+function Respect() {
+  return (
+    <div className="main-container">
+      <DropdownMenu
+        title="Respect"
+        content="
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      />
+    </div>
+  )
+}
+
+function Service() {
+  return (
+    <div className="main-container">
+      <DropdownMenu
+        title="Service"
+        content="
+        Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+      />
+    </div>
+  )
+}
+
+function Sécurité() {
+  return (
+    <div className="main-container">
+      <DropdownMenu
+        title="Sécurité"
+        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet ànos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+      />
+    </div>
+  )
+}
+
+
+
+
 
 function APropos() {
   return (
@@ -10,6 +61,10 @@ function APropos() {
       <div className='margin'>
     <Header />
     <img className='pic-header' src={imgapropos} alt="slogan"/>
+    <Fiabilité />
+    <Respect />
+    <Service />
+    <Sécurité/>
     </div>
     <Footer />
     </div>
